360ea12ae1b9a4d92e5605b71204394f1eafba1a
Pawel Borowik <pawelbor1@op.pl>
2020-04-09 23:42:29 +0200

    add random to app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+
+  const [cards, setCards]= useState(["red", "red","brown", "brown", "green", "green", "blue", "blue", 
+  "yellow", "yellow", "orangered", "orangered", "violet", "violet", "steelblue", "steelblue"])
+  
+ 
+      const iii = cards.map( card =>{
+        const position =Math.floor(Math.random()*cards.length)
+        // setCards(cards.splice(position, 1))
+        return(
+          <div className={`card ${cards[position]}`}></div>
+        )
+        }
+      )
+    
+     
+  
+    
+      
+
   return (
     <div className="container">
+      {/* <button onClick={()=>handleClick()}>Start</button> */}
     <div className="App">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+          
+        {iii}
+      
         </div>
     </div>
+    
   );
+  
 }
 
 export default App;
